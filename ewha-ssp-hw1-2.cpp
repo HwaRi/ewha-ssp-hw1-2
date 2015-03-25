b0581575ebf00c45065053ef55e03ff92f25e6db
RihoHwa <hana9272@gmail.com>
2015-03-25 11:30:24 +0900

    change cpp file
    changed file into lab2-2

--- ewha-ssp-hw1-2.cpp
+++ ewha-ssp-hw1-2.cpp
@@ -1,35 +1,50 @@
 // Do not remove the include below
 #include "ewha-ssp-hw1-2.h"
-
-#define FRONT_LED_PIN 10
+// Do not remove the include below
 #define REAR_LED_PIN 9
+#define DUTY_CYCLE 10
 
+bool checker = true;
 //The setup function is called once at startup of the sketch
+void PWM_Write(int pin, int on_time_perc){
+	int on_time = DUTY_CYCLE*on_time_perc/100;
+	int off_time = DUTY_CYCLE*(1-(on_time_perc/100));
+
+	digitalWrite(REAR_LED_PIN, HIGH);
+	delay(on_time);
+	digitalWrite(REAR_LED_PIN, LOW);
+	delay(off_time);
+
+}
+
+void myAnalogWrite(int pin, int percent, int time){
+	int numLoops = time/DUTY_CYCLE;
+	int i;
+
+	for(i = 0; i<numLoops;i++){
+		PWM_Write(pin, percent);
+	}
+}
+
 void setup()
 {
 // Add your initialization code here
-
-	pinMode(FRONT_LED_PIN, OUTPUT);
 	pinMode(REAR_LED_PIN, OUTPUT);
-	digitalWrite(FRONT_LED_PIN, LOW);
 	digitalWrite(REAR_LED_PIN, HIGH);
-
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-
-	for(int value = 0 ; value <= 255; value +=1) {
-	    analogWrite(FRONT_LED_PIN, value);
-	    analogWrite(REAR_LED_PIN, 255-value);
-	    delay(20);
-	  }
-	for(int value = 255 ; value >= 0; value -=1) {
-	    // sets the value (range from 0 to 255):
-	    analogWrite(FRONT_LED_PIN, value);
-	    analogWrite(REAR_LED_PIN, 255-value);
-	    delay(20);
-	  }
+	while(checker == true){
+		checker = false;
+		delay(2000);
+		myAnalogWrite(REAR_LED_PIN, 75, 2000);
+		myAnalogWrite(REAR_LED_PIN, 50, 2000);
+		myAnalogWrite(REAR_LED_PIN, 25, 2000);
+		//myAnalogWrite(REAR_LED_PIN, 0, 2000);
+		digitalWrite(REAR_LED_PIN, LOW);
+		break;
+	}
+//Add your repeated code here
 }
-
